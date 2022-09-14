04d1e1670f8bb6644dc3b4affefa3c425c46624e
Serge-repository <58727013+Serge-repository@users.noreply.github.com>
2022-09-14 15:14:21 +0300

    edit

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -4,10 +4,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         // Uncomment for local run
 //        tags = "@Smoke or @DragDrop",           //указав тег, можем тут же запускать ЛОКАЛЬНО через RunnerTest
-        features = "src/test/resources/features", // Можно указать несколько папок, например: features = {«src/test/features», «src/test/feat»}
-        // Uncomment for selenium grid run
-//        features = "classpath:features",
 
+        features = "src/test/resources/features", // Можно указать несколько папок, например: features = {«src/test/features», «src/test/feat»}
         glue = "cucumber_step_defs",  // Можно указать несколько пакетов, например, так: glue = {«ru.savkk.test», «ru.savkk.hooks»}
         plugin = {"pretty", "html:target/cucumber-reports/html_reports",
                 "json:target/cucumber-reports/CucumberTestReport.json"
